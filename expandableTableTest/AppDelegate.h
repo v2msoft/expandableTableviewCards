b4f6e868f3854185f8da469611be2140e551a6fa
Manel Roca <manel.roca@v2msoft.com>
2018-01-25 13:08:57 +0100

    Initial Commit

---
+++ expandableTableTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  expandableTableTest
+//
+//  Created by projectes on 25/1/18.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
